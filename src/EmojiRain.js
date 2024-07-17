18deb543f07f0773f344a04698fa8f506f8067de
Omar Shehata <omar.sameh.shehata@gmail.com>
2024-07-17 18:11:41 -0400

    remove placeholders

--- src/EmojiRain.js
+++ src/EmojiRain.js
@@ -1,16 +1,16 @@
 var container = document.getElementById('emoji-rain');
-var emoji = ['😀', '😁', '😄', '😊', '😎', '😍', '🙂', '🤗', '🤩', '😝', '🥳', '🤠', '😸', '🐸', '🐼', '🐨', '🐔', '🐌', '🐝', '🕺', '💃', '🙌', '🫶', '👋', '👯', '🎈', '✨', '🎉', '🎊', '🏆', '🪩', '🪇', '🍺', '🍻', '🥂', '🍾', '🌸', '🏵️', '🌺', '🪂', '🏘️', '🌝', '🌞', '🌟', '⭐', '🌈', '💖', '💫', '💯', '⭕'];
+var emoji = ['😀', '😁', '😄', '😊', '😎', '😍', '🙂', '🤗', '🤩', '😝', '🥳', '🤠', '😸', '🐸', '🐼', '🐨', '🐔', '🐌', '🐝', '🕺', '💃', '🙌', '🫶', '👋', '👯', '🎈', '✨', '🎉', '🎊', '🏆', '🪩', '🪇', '🍺', '🍻', '🥂', '🍾', '🌸', '🌺', '🪂', '🏘️', '🌝', '🌞', '🌟', '⭐', '🌈', '💖', '💫', '💯', '⭕'];
 var circles = [];
 
 
 
 
-function addCircle(delay, range, color) {
+function addCircle(delay, range, color, Y = -30) {
   setTimeout(function() {
     const xSpeed = 0.15
-    var c = new Circle(range[0] + Math.random() * range[1], -30, color, {
+    var c = new Circle(range[0] + Math.random() * range[1], Y, color, {
       x: (-xSpeed * 0.5) + Math.random() * xSpeed,
-      y: 0.1 + Math.random() * 0.2
+      y: 0.05 + Math.random() * 0.1
     }, range);
     circles.push(c);
   }, delay);
@@ -30,6 +30,7 @@ function Circle(x, y, c, v, range) {
   this.element.style.fontSize = '26px';
   this.element.style.color = 'hsl('+(Math.random()*360|0)+',80%,50%)';
   this.element.innerHTML = c;
+  this.initialized = false
   container.appendChild(this.element);
 
   this.update = function(delta) {
@@ -39,7 +40,12 @@ function Circle(x, y, c, v, range) {
     }
     _this.y += _this.v.y * delta;
     _this.x += _this.v.x * delta;
-    this.element.style.opacity = 1;
+    
+    if (!this.initialized) {
+        this.initialized = true 
+        this.element.style.opacity = 1;
+    }
+    
     this.element.style.transform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
     this.element.style.webkitTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
     this.element.style.mozTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)';
@@ -47,6 +53,8 @@ function Circle(x, y, c, v, range) {
 }
 
 var start = null
+var intervalId = null
+
 function animate(timestep) {
     if (start == null) {
         start = timestep
@@ -57,23 +65,35 @@ function animate(timestep) {
    for (var i in circles) {
     circles[i].update(elapsed);
   }
+
   requestAnimationFrame(animate);
 }
 
 
-
-
 function startEmojiRain() {
     for (var i = 0; i < 15; i++) {
-
         const width = window.innerWidth 
-        const step = width / 20
-        for (let x = 0; x < width; x += 200) {
-          addCircle(i * 150, [x, 300], emoji[Math.floor(Math.random() * emoji.length)]);
+        const height = window.innerHeight 
+        const step = width / 5
+        for (let x = 0; x < width; x += step) {
+          addCircle(i * 150, [x, 300], emoji[Math.floor(Math.random() * emoji.length)], -30 - height * Math.random());
         }
       }
       
     animate();
+
+    clearTimeout(intervalId)
+    intervalId = setTimeout(() => {
+        document.querySelector("#emoji-rain").classList.add("fadeout-element")
+        intervalId = setTimeout(() => {
+            for (let i = 0 ;i < circles.length; i++) {
+                container.removeChild(circles[i].element);
+            }
+            circles = []
+            document.querySelector("#emoji-rain").classList.remove("fadeout-element")
+
+        }, 2000)
+        
+    }, 15000)
 }
 window.startEmojiRain = startEmojiRain
-// startEmojiRain()
