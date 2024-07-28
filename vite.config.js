0cf86ea93e5d4166cc9471111faa0f2a28f916a5
Omar Shehata <omar.sameh.shehata@gmail.com>
2024-07-28 15:27:45 -0400

    fix about page link

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: "./",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
+  },
+})
